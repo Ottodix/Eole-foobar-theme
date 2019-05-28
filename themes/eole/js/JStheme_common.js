c81558753487c3ac9a8b633bffafaa7a63d2b562
Ottodix <colas.generau@gmail.com>
2019-05-28 18:04:32 +0200

    v1.04

--- themes/eole/js/JStheme_common.js
+++ themes/eole/js/JStheme_common.js
@@ -20,7 +20,7 @@ var last_mouse_move_notified = (new Date).getTime();
 var foo_playcount = utils.CheckComponent("foo_playcount", true);
 
 var globalProperties = {
-	theme_version: '1.0.4b',
+	theme_version: '1.0.4',
     thumbnailWidthMax: 300,
     coverCacheWidthMax: 400,
 	TextRendering: 4,
