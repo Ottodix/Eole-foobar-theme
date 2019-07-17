2cf4fe3bf5d69c8e466c26c53af5de08147aa380
Ottodix <colas.generau@gmail.com>
2019-07-17 12:37:12 +0200

    Update WSHsmoothplaylist.js

--- themes/eole/js/WSHsmoothplaylist.js
+++ themes/eole/js/WSHsmoothplaylist.js
@@ -1628,7 +1628,9 @@ oBrowser = function(name) {
                 this.nowplaying = plman.GetPlayingItemLocation();
                 if(this.nowplaying.IsValid) {
                     if(plman.PlayingPlaylist != g_active_playlist) {
-						if(!properties.lockOnPlaylistNamed=="") return;
+						if(!properties.lockOnPlaylistNamed=="") {
+							return;
+						}
                         g_active_playlist = plman.ActivePlaylist = plman.PlayingPlaylist;						
 						this.showNowPlaying_trigger = true;
 						this.populate(is_first_populate = true,21);
