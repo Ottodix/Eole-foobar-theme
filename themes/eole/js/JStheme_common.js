ed90ad2d2f389734492d4fb759373565cc86b620
Ottodix <colas.generau@gmail.com>
2019-08-03 15:22:28 +0200

    Update JStheme_common.js

--- themes/eole/js/JStheme_common.js
+++ themes/eole/js/JStheme_common.js
@@ -22,8 +22,8 @@ var last_mouse_move_notified = (new Date).getTime();
 var foo_playcount = utils.CheckComponent("foo_playcount", true);
 var timers = []
 var globalProperties = {
-	theme_version: '1.2.2RC2',
-	lastest_breaking_version: '1.2.2RC2',	
+	theme_version: '1.2.3b1',
+	lastest_breaking_version: '1.2.3b1',	
     thumbnailWidthMax: window.GetProperty("GLOBAL thumbnail width max", 200),
     coverCacheWidthMax: window.GetProperty("GLOBAL cover cache width max", 400),
 	TextRendering: 4,
