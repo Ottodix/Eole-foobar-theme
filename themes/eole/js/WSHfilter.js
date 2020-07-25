77b33429935b1d92a11563c0c450ddc90b4a4a3c
Ottodix <colas.generau@gmail.com>
2020-07-25 23:08:15 +0200

    Update WSHfilter.js

--- themes/eole/js/WSHfilter.js
+++ themes/eole/js/WSHfilter.js
@@ -129,9 +129,10 @@ var properties = {
     addedRows_end_default: window.GetProperty("_PROPERTY: empty rows at the end", 1),
 	panelFontAdjustement: -1,
 	load_image_from_cache_direct: true,
-	adapt_to_playlist: false	
+	adapt_to_playlist: true	
 };
-
+if(properties.adapt_to_playlist && properties.ParentName=="Library") properties.adapt_to_playlist = false;
+	
 var TF = {
 	genre: fb.TitleFormat('%genre%'),
 	albumartist: fb.TitleFormat("%album artist%"),
