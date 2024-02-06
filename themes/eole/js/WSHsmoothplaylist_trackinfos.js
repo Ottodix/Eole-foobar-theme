a785770039cc3a30f514b00e285996f52a3f858e
JadeTank <35437313+JadeTank@users.noreply.github.com>
2024-02-06 08:20:26 -0500

    Change delimiter to use regex

--- themes/eole/js/WSHsmoothplaylist_trackinfos.js
+++ themes/eole/js/WSHsmoothplaylist_trackinfos.js
@@ -1903,7 +1903,11 @@ oBrowser = function(name) {
         var str_filter = process_string(filter_text);
 		for(var i = 0; i < total; i++) {
 			handle = this.list[i];
-            arr = tf.EvalWithMetadb(handle).split(" ## ");
+			console.log(tf.EvalWithMetadb(handle));
+            arr = tf.EvalWithMetadb(handle).split(/ ## (.*)/);
+			console.log("[0] " + arr[0]); 
+			console.log("[1] " + arr[1]); 
+			console.log("[2] " + arr[2]); 
             current = arr[0].toLowerCase();
             if(str_filter.length > 0) {
                 var toAdd = match(arr[0]+" "+arr[1], str_filter);
